dd7581eac9a8a3a9971b599d661196a15f5cb9d7
Yaroslav Pisarev <yaricp@gmail.com>
2020-10-23 22:12:42 +0700

    upgrade installation and upgrading

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 sudo apt install -y curl python3 python3-pip qt5-default
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-export PATH=$PATH:$HOME/.poetry/bin
+pip3 install poetry
+export PATH=$PATH:$HOME/.local/bin
 poetry config virtualenvs.in-project true
 poetry install
 
-#python3 -m venv venv3
-#venv3/bin/pip install pip --upgrade
-#venv3/bin/pip install -r requirements.txt
 
--- upgrade.sh
+++ upgrade.sh
@@ -2,5 +2,4 @@
 
 git stash
 git pull
-poetry install
-#venv3/bin/pip install -r requirements.txt --upgrade
+./install.sh
