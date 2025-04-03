53930375bf49f7656e5f3e5aae5f2606c12575f6
Yaroslav Pisarev <yaricp@gmail.com>
2025-04-03 23:53:20 +0200

    fix bugs with qt5_waiting_spinner.py

--- src/qt5_waiting_spinner.py
+++ src/qt5_waiting_spinner.py
@@ -27,11 +27,9 @@ SOFTWARE.
 
 import math
 
-from PyQt5.QtCore import Qt
-# from PyQt5.QtGui import *
-from PyQt5.QtWidgets import (
-    QWidget, QColor, QPainter, QTimer, QRect
-)
+from PyQt5.QtCore import Qt, QTimer, QRect
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtWidgets import QWidget
 
 
 class QtWaitingSpinner(QWidget):
