7609956626021bf9f29e7f609b2985203549494d
Yaroslav Pisarev <yaricp@gmail.com>
2025-04-05 22:15:47 +0200

    added autolinters and auto building docker images

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt update -y;
-apt install -y curl python3 python3-pip python3-pyqt5
-pip3 install poetry
-cd ../
-export PATH=$PATH:$HOME/.local/bin
-echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
-poetry config virtualenvs.in-project true
-poetry install
+sudo apt update -y;
+sudo apt install -y curl python3 python3-pip python3-pyqt5;
+pip3 install poetry;
+cd ../;
+export PATH=$PATH:$HOME/.local/bin;
+echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc;
+poetry config virtualenvs.in-project true;
+poetry install;
 
 
