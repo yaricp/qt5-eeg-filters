8b0b3a8d1294c0107cd7826b1030f5f04241b45d
Yaroslav Pisarev <yaricp@gmail.com>
2025-04-19 13:55:32 +0200

    added scripts for installation on linux and macos. made one start.sh file

--- scripts/start_in_docker_container.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-export DISPLAY=:0.0
-xhost +
-if [ "${PWD##*/}" == "scripts" ]; then
-  cd ../;
-fi
-docker compose pull
-docker compose up -d
---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ./scripts/linux/install.sh;
+    cp .env_linux .env;
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    ./scripts/macos/install.sh;
+    cp .env_macos .env;
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "cygwin not supported yet!"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    echo "msys not supported yet!"
+elif [[ "$OSTYPE" == "win32" ]]; then
+    echo "win32 not supported yet!"
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    echo "freebsd not supported yet!"
+fi
+
+docker compose pull;
+export DISPLAY=:0.0 ;
+xhost + ;
+docker compose up -d;
